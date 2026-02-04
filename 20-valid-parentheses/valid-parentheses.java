e33bcc3f93a2b89b93e9fea918339fe35aaecdb9
Guru Swathi Durvesi Mulinti <swathidm49@gmail.com>
2026-02-04 19:05:39 +0530

    Time: 3 ms (87.85%) | Memory: 43.2 MB (83.40%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[')
+                st.push(c);
+            else {
+                if (st.isEmpty()) return false;
+                char t = st.pop();
+                if (c == ')' && t != '(') return false;
+                if (c == '}' && t != '{') return false;
+                if (c == ']' && t != '[') return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
