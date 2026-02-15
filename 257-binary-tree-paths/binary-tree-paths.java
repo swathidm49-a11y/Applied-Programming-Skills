157e92ca372228b1f0799a75f770f0f0d18a86d6
Guru Swathi Durvesi Mulinti <swathidm49@gmail.com>
2026-02-15 13:48:09 +0530

    Time: 5 ms (41.20%) | Memory: 50.1 MB (19.39%) - LeetSync

---
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        dfs(root, "", res);
+        return res;
+    }
+    
+    void dfs(TreeNode node, String path, List<String> res) {
+        if (node == null) return;
+        if (!path.isEmpty()) path += "->";
+        path += node.val;
+        if (node.left == null && node.right == null) {
+            res.add(path);
+            return;
+        }
+        dfs(node.left, path, res);
+        dfs(node.right, path, res);
+    }
+}
