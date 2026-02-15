e4bb1d17ea2f5ba5c547da672761243396c4e6c1
Guru Swathi Durvesi Mulinti <swathidm49@gmail.com>
2026-02-15 13:40:38 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (50.40%) - LeetSync

---
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        preorder(root, list);
+        return list;
+    }
+    
+    private void preorder(TreeNode node, List<Integer> list) {
+        if (node == null) return;
+        list.add(node.val);
+        preorder(node.left, list);
+        preorder(node.right, list);
+    }
+}
