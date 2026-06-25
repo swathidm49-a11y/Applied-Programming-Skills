90e873f9402d53d46cded247e25aa117a77e88f9
Guru Swathi Durvesi Mulinti <swathidm49@gmail.com>
2026-06-25 09:22:52 +0530

    Time: 34 ms (66.77%) | Memory: 47.1 MB (40.92%) - LeetSync

---
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countMajoritySubarrays(int[] nums, int target) {
+        int n = nums.length;
+        int totalSubarrays = 0;
+        
+        for (int i = 0; i < n; i++) {
+            int targetCount = 0;
+            for (int j = i; j < n; j++) {
+                if (nums[j] == target) {
+                    targetCount++;
+                }
+                int len = j - i + 1;
+                if (targetCount * 2 > len) {
+                    totalSubarrays++;
+                }
+            }
+        }
+        
+        return totalSubarrays;
+    }
+}
